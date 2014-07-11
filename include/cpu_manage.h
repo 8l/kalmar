aa73812176b61afb2443ca98c5dfdd5bd4de7f50
UncleHandsome <yunwei@multicorewareinc.com>
2014-07-11 14:25:03 +0800

    Clean up cpu_manage

--- include/cpu_manage.h
+++ include/cpu_manage.h
@@ -9,6 +9,7 @@
 #pragma once
 #include <malloc.h>
 #include <string.h>
+#include <cstdio>
 namespace Concurrency {
 
     template<class T>
@@ -33,8 +34,6 @@ namespace Concurrency {
             _data_host(const _data_host &other):std::shared_ptr<T>(other) {}
             _data_host(std::nullptr_t x = nullptr):std::shared_ptr<T>(nullptr) {}
         };
-    // Wrap a shared pointer to the gmac buffer and the cache state. Act
-    // as if a shared pointer to the gmac buffer itself.
     template <typename T>
         class _data_host_view {
         private:
@@ -46,9 +45,6 @@ namespace Concurrency {
             __attribute__((cpu)) size_t buffer_size;
 
         public:
-            _data_host_view(nc_T* cache, T* home, size_t size) :
-                gmac_buffer(cache), home_ptr(home), buffer_size(size) {}
-
             template <class Deleter>
                 _data_host_view(nc_T* cache, Deleter d, T* home, size_t size) :
                     gmac_buffer(cache, d), home_ptr(home), buffer_size(size) {}
@@ -75,49 +71,25 @@ namespace Concurrency {
                     gmac_buffer(other.gmac_buffer),
                     home_ptr(other.home_ptr), buffer_size(other.buffer_size) {}
 
-            void reset() {
-                gmac_buffer.reset();
-            }
-
+            void reset() { gmac_buffer.reset(); }
             void refresh() const {}
-
-            //If the cache is currently owned by the gmac buffer, copy it back to the host
-            //buffer and change the state to shared.
             void synchronize() const {}
-
-            //Check the gmac buffer use count to see if we are the last copy of it. This
-            //is used to check if we are the last copy of an array_view and need to
-            //implicitly synchronize.
             bool is_last() const { return gmac_buffer.unique(); }
 
-            //Return the home location ptr, synchronizing first if necessary. The pointer
-            //returned is mutable, so we set it as host owned. If this is an array or array_view
-            //without a host buffer just return a pointer to the gmac buffer.
             T* get_mutable() const {
-                if (home_ptr) {
-                    synchronize();
+                if (home_ptr)
                     return home_ptr;
-                } else {
+                else
                     return gmac_buffer.get();
-                }
             }
-            T* get() const {
-                return get_mutable();
-            }
-            T* get_data() const {
-                return get_mutable();
-            }
+            T* get() const { return get_mutable(); }
+            T* get_data() const { return get_mutable(); }
 
-            //Return the home location ptr, synchronizing first if necessary. The pointer
-            //returned is const, so we can leave the cache in a shared state. If this is
-            //an array or array_view without a host buffer just return a pointer to the gmac buffer.
             const T* get() {
-                if (home_ptr) {
-                    synchronize();
+                if (home_ptr)
                     return home_ptr;
-                } else {
+                else
                     return gmac_buffer.get();
-                }
             }
         };
 } // namespace Concurrency
